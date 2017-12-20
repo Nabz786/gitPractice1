ec336a47750f04ca02c1e021318eb70215eec562
Nabz786 <nabvali98@gmail.com>
2017-12-20 06:55:45 -0500

    This is my second commit, just a simple comment

---
+++ src/Practice.java
@@ -0,0 +1,6 @@
+public class Practice {
+    public static void main(String[] args) {
+        System.out.print("I'm Learning git");
+        // This is a test comment. I'm practicing with Git!
+    }
+}
